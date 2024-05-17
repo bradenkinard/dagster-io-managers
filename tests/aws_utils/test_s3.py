50adeeddde442169f732ba541f65ccedeb3defd0
Braden Kinard <23621962+bradenkinard@users.noreply.github.com>
2024-05-17 15:17:04 -0400

    refactor s3 utils tests

--- tests/aws_utils/test_s3.py
+++ tests/aws_utils/test_s3.py
@@ -1,7 +1,7 @@
 """Tests for the aws module."""
 
 import boto3
-from dagster_io_managers.aws_utils.s3 import create_bucket, get_bucketpath, get_filepath
+from dagster_io_managers.aws_utils import s3
 from moto import mock_aws
 
 
@@ -10,7 +10,7 @@ def test_create_bucket_no_region(aws_credentials):
     """Test initializing S3Repository without a region."""
     bucket_name = "my-test-bucket"
     s3_client = boto3.client("s3")
-    create_bucket(bucket_name=bucket_name)
+    s3.create_bucket(bucket_name=bucket_name)
 
     # Check if the bucket was created
     response = s3_client.list_buckets()
@@ -24,27 +24,25 @@ def test_create_bucket_with_region(aws_credentials):
     bucket_name = "my-test-bucket"
     region = "us-west-1"
     s3_client = boto3.client("s3")
-    create_bucket(bucket_name=bucket_name, region=region)
+    s3.create_bucket(bucket_name=bucket_name, region=region)
 
     # Check if the bucket was created in the specified region
     response = s3_client.get_bucket_location(Bucket=bucket_name)
     assert response["LocationConstraint"] == region
 
 
-@mock_aws
 def test_basepath(aws_credentials):
     """Test the basepath property."""
     bucket_name = "my-test-bucket"
     expected_path = f"s3://{bucket_name}"
 
-    assert get_bucketpath(bucket_name=bucket_name) == expected_path
+    assert s3.get_bucketpath(bucket_name=bucket_name) == expected_path
 
 
-@mock_aws
 def test_filepath(aws_credentials):
     """Test the filepath method."""
     bucket_name = "my-test-bucket"
     key = "some/key"
     expected_path = f"s3://{bucket_name}/{key}"
 
-    assert get_filepath(bucket_name=bucket_name, key=key) == expected_path
+    assert s3.get_filepath(bucket_name=bucket_name, key=key) == expected_path
